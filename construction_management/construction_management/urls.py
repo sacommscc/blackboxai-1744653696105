8bd8fbfeb5e4bfc70b2527fc4ce2d2e54a939446
S.Ali <60627293+sacommscc@users.noreply.github.com>
2025-04-16 20:42:21 +0530

    Add construction_management/construction_management/urls.py 1744816340987

--- construction_management/construction_management/urls.py
+++ construction_management/construction_management/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
+    path('labour/', include('labour.urls', namespace='labour')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
