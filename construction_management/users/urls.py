4b0ee88d12afef8a02df6fdee99ad92ba92737d8
S.Ali <60627293+sacommscc@users.noreply.github.com>
2025-04-19 18:32:29 +0530

    Add construction_management/users/urls.py 1745067748867

--- construction_management/users/urls.py
+++ construction_management/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
