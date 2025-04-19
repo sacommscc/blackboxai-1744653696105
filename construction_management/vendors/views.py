2e2966778e7f8429d1a3858e546dd465c4d25be1
S.Ali <60627293+sacommscc@users.noreply.github.com>
2025-04-19 14:35:57 +0530

    Add construction_management/vendors/views.py 1745053557475

--- construction_management/vendors/views.py
+++ construction_management/vendors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Vendor
+from .serializers import VendorSerializer
 
-# Create your views here.
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Optionally filter by user or other criteria
+        return super().get_queryset()
